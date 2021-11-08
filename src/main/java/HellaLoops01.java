486c8c6099d9c7dcd5618f393dc21d5b3ddf5e0f
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-11-08 15:58:55 -0500

    first commit

---
+++ src/main/java/HellaLoops01.java
@@ -0,0 +1,33 @@
+public class HellaLoops01 {
+    public static String getEvenNumbers(int start, int stop) {
+        return null;
+    }
+
+
+    public static String getOddNumbers(int start, int stop) {
+        return null;
+    }
+
+
+    public static String getSquareNumbers(int start, int stop, int step) {
+        return null;
+    }
+
+    public static String getRange(int start) {
+        return null;
+    }
+
+    public static String getRange(int start, int stop) {
+        return null;
+    }
+
+
+    public static String getRange(int start, int stop, int step) {
+        return null;
+    }
+
+
+    public static String getExponentiations(int start, int stop, int step, int exponent) {
+        return null;
+    }
+}
---
+++ src/test/java/HellaLoops01Test.java
@@ -0,0 +1,205 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HellaLoops01Test {
+
+    private HellaLoops01 loops;
+
+    @BeforeEach
+    public void setUp(){
+        loops  = new HellaLoops01();
+    }
+
+    @Test
+    public void testGetRange1A() {
+        // : Given
+        String expected = "0123456789";
+        int stop = 11;
+
+        // : When
+        String actual = loops.getRange(stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetRange2A() {
+        // : Given
+        String expected = "01234";
+        int stop = 5;
+
+        // : When
+        String actual = loops.getRange(stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetRange3A() {
+        // : Given
+        String expected = "012345678910111213141516171819";
+        int stop = 20;
+
+        // : When
+        String actual = loops.getRange(stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetRange1B() {
+        // : Given
+        String expected = "5678910111213141516171819";
+        int start = 5;
+        int stop = 20;
+
+        // : When
+        String actual = loops.getRange(start, stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testGetRange2B() {
+        // : Given
+        String expected = "101112131415161718192021222324";
+        int start = 10;
+        int stop = 25;
+
+        // : When
+        String actual = loops.getRange(start, stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testGetRange3B() {
+        // : Given
+        String expected = "100101103104105106107108109";
+        int start = 100;
+        int stop = 110;
+
+        // : When
+        String actual = loops.getRange(start, stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetRange1C() {
+        // : Given
+        String expected = "51015";
+        int start = 5;
+        int stop = 20;
+        int step = 5;
+
+        // : When
+        String actual = loops.getRange(start, stop, step);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testGetRange2C() {
+        // : Given
+        String expected = "012345678910111213141516171819";
+        int start = 0;
+        int stop = 20;
+        int step = 1;
+
+        // : When
+        String actual = loops.getRange(start, stop, step);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testGetRange3C() {
+        // : Given
+        String expected = "0246810";
+        int start = 0;
+        int stop = 11;
+        int step = 2;
+
+        // : When
+        String actual = loops.getRange(start, stop, step);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetEvenNumbers() {
+        // : Given
+        String expected = "5791113151719";
+        int start = 5;
+        int stop = 20;
+
+        // : When
+        String actual = loops.getEvenNumbers(start, stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetOddNumbers() {
+        // : Given
+        String expected = "681012141618";
+        int start = 5;
+        int stop = 20;
+        int step = 5;
+
+        // : When
+        String actual = loops.getOddNumbers(start, stop);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    public void testGetSquareNumbers() {
+        // : Given
+        String expected = "25100225";
+        int start = 5;
+        int stop = 20;
+        int step = 5;
+
+        // : When
+        String actual = loops.getSquareNumbers(start, stop, step);
+
+        // : Then
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testGetExponentiationNumbers() {
+        // : Given
+        String expected = "25100225";
+        int start = 5;
+        int stop = 20;
+        int step = 5;
+        int exponent = 2;
+
+        // : When
+        String actual = loops.getExponentiations(start, stop, step, exponent);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
