235363b1d871abef068db85a441f4a9472d3a80c
tatianastaicova <staicova.tatiana@gmail.com>
2021-11-09 09:15:02 -0500

    HellaLoops01 is a hella

--- src/main/java/HellaLoops01.java
+++ src/main/java/HellaLoops01.java
@@ -1,33 +1,55 @@
 public class HellaLoops01 {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String answer = "";
+        for(int i = start; i<stop; i++){
+            if(i%2 !=0){
+                answer +=i;
+            }
+        }
+
+        return answer;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String answer = "";
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                answer += i;
+            }
+        }
+
+
+        public static String getSquareNumbers ( int start, int stop, int step){
+            String answer1 = "";
+            for (int i = start; i < stop; i += start) {
+                answer1 += (i * i);
+            }
+            return answer1;
+        }
+
+        public static String getRange ( int stop){
+            String response = "";
+            for (int i = 0; i < stop; i++)
+                response += i;
+            return response;
+        }
+
+        public static String getRange ( int start, int stop){
+            return null;
+        }
+
+
+        public static String getRange ( int start, int stop, int step){
+            return null;
+        }
+
+
+        public static String getExponentiations (int start, int stop, int step, int exponent){
+            String answer2 = "";
+            for (int i = start; i < stop; i += step) {
+                answer2 += (int) Math.pow(i, exponent);
+            }
+            return answer2;
+        }
     }
-
-
-    public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
-    }
-
-    public static String getRange(int start) {
-        return null;
-    }
-
-    public static String getRange(int start, int stop) {
-        return null;
-    }
-
-
-    public static String getRange(int start, int stop, int step) {
-        return null;
-    }
-
-
-    public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
-    }
-}
