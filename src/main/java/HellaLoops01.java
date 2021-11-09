22578c557c15b032bc93fba240ab0b3f168708f5
Kris Sanchez <ksanchez436@gmail.com>
2021-11-09 09:02:53 -0500

    first

--- src/main/java/HellaLoops01.java
+++ src/main/java/HellaLoops01.java
@@ -1,33 +1,72 @@
 public class HellaLoops01 {
-    public static String getEvenNumbers(int start, int stop) {
-        return null;
-    }
 
 
-    public static String getOddNumbers(int start, int stop) {
-        return null;
-    }
+    public static String getRange(int stop) {
+        String response = "";
+        for(int i = 0; i<stop; i++){
 
+            response+=i;}
 
-    public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
-    }
-
-    public static String getRange(int start) {
-        return null;
+        return response;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String response = "";
+        for(int i =start; i<stop; i++){
+            response+=i;
+        }
+
+
+        return response;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String response = "";
+        for(int i =start; i<stop; i+=step){
+            response+=i;
+        }
+        return response;
+    }
+
+    public static String getEvenNumbers(int start, int stop) {
+        String response="";
+        for (int i = start; i<stop;i+=2){
+            response+=i;
+        }
+        return response;
+    }
+
+    public static String getOddNumbers(int start, int stop) {
+        String response="";
+        for (int i = start; i<stop;i+=2){
+            response+=i;
+        }
+
+        return response;
     }
 
 
+    public static String getSquareNumbers(int start, int stop, int step) {
+        String response="";
+        for (int i = start; i<stop;i+=step){
+            Math.pow(i,i)
+        }
+
+        }
+
+
+            return response;
+    }
+
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String response = "";
+
+        for (int i = start; i<stop;i+=step){
+            Math.pow(i,exponent);
+
+
+        return response;
+
     }
 }
