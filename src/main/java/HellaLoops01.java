08f5c1036af998e6fac28a22cde63398f2162892
Michiah <85957194+Michiah@users.noreply.github.com>
2021-11-09 08:56:56 -0500

    working

--- src/main/java/HellaLoops01.java
+++ src/main/java/HellaLoops01.java
@@ -1,11 +1,23 @@
 public class HellaLoops01 {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String[] evenNums;
+        for (int i = start;i< stop; i++){
+            if(i%2==0){
+
+            }
+        }
+        return
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        int oddNums = 0;
+        for (int i = start;i< stop; i++){
+            if(i%2 !=0){
+                oddNums = i;
+            }
+        }
+        return
     }
 
 
@@ -23,7 +35,10 @@ public class HellaLoops01 {
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range;
+        for (int i = start; i < stop; i+=step)
+
+        return
     }
 
 
